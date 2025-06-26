371e6a5761141916bb6268918281a221ff7a95aa
GregoryBiter <gregorybiter@gmail.com>
2025-06-26 16:54:54 +0300

    Добавлены файлы тестового модуля и его хуков для системы обновлений; добавлен архив с модулем версии 1.0.1.

---
+++ test_module/upload/admin/controller/extension/module/test_module.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Test Module Controller
+ * 
+ * @author Test Author
+ * @version 1.0.1
+ * @description Test module for update system
+ * @author_url https://example.com
+ */
+
+class ControllerExtensionModuleTestModule extends Controller {
+    
+    public function index() {
+        echo "Test Module v1.0.1";
+    }
+}
